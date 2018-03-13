3867eeb8bab389b4df802ec6f25815b666812199
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2018-03-13 17:29:28 +0100

    add finddiff.sh

---
+++ finddiff.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#create tmp directory
+mkdir -p /tmp/finddiff
+
+#first find
+cd $1
+find ./ | sort -d > /tmp/finddiff/finddiff1.txt
+cd - > /dev/null
+
+#second find
+cd $2
+find ./ | sort -d > /tmp/finddiff/finddiff2.txt
+cd - > /dev/null
+
+#diff
+diff -u /tmp/finddiff/finddiff*
+
+#remove tmp directory
+rm -fr /tmp/finddiff
