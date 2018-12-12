ac989633e77c4bf46613070ebd516c7fe0be566c
Erwan Chériaux <dirtwan@hotmail.fr>
2018-12-12 14:01:02 +0100

    multiple card

--- entrustcard.py
+++ entrustcard.py
@@ -1,27 +1,33 @@
 import os
 
 class Card:
-    def __init__(self, name, data):
+    def __init__(self, name, file_path):
         self.name = name
-        self.data = data
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as file:
+                self.data = file.readlines()
+            self.isValid = True
+        else:
+            print(file_path + " not found")
+            self.isValid = False
 
     def printCode(self, coordinates):
-        if not isValideCoordinatesLength(coordinates):
+        if not self.isValideCoordinatesLength(coordinates):
             print("Wrong coordinates length: " + coordinates)
         else:
             i = 0
             code = ''
             while i < len(coordinates):
-                c = getChar(coordinate[i:i+1])
+                c = self.getChar(coordinates[i:i+2])
                 if c:
                     code = code + c
                 i = i + 2
 
             if len(coordinates) == len(code)*2:
-                print("card n°" + name + ": " + code)
+                print("card n°" + self.name + ": " + code)
 
     def getChar(self, coordinate):
-        if isValidColumn(coordinate[0]) and isValidRow(coordinate[1]):
+        if self.isValidColumn(coordinate[0]) and self.isValidRow(coordinate[1]):
             column = ord(coordinate[0].lower()) - ord('a')
             row = int(coordinate[1]) - 1
             return self.data[row][column]
@@ -47,35 +53,19 @@ class Card:
         else:
             return False
 
+    def isValid(self):
+        return self.isValid
+
+#############################################################
+
 if hasattr(__builtins__, 'raw_input'):
     input=raw_input
 
-file_path = "card.txt"
+cards = []
+cards.append(Card("14268", "card14268.txt"))
+cards.append(Card("14407", "card14407.txt"))
 
-# check first if file exsits
-if not os.path.exists(file_path):
-   print(file_path + " not found")
-else:
-    with open(file_path, 'r') as file:
-        file_data = file.readlines()
+coordinates = input("Entre card coordinates: ")
 
-    strin = input("Entre card coordinates: ")
-
-    # check user input
-    if len(strin)%2:
-        print(strin + " are wrong coordinates")
-    else:
-        i = 0
-        strout = ''
-        while i<len(strin):
-            if not ('a' <= strin[i].lower() <= 'j') or not ('1' <= strin[i+1] <= '5'):
-                print(strin[i] + strin[i+1] + " are wrong coordinates")
-            else:
-                column = ord(strin[i].lower()) - ord('a')
-                row = int(strin[i+1]) - 1
-                strout = strout + file_data[row][column]
-
-            i = i+2
-
-        if len(strin) == len(strout)*2:
-            print("Code decrypted: " + strout)
+for card in cards:
+    card.printCode(coordinates)
